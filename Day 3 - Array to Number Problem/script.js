f44940ee7abfe65bdc886e4d7ddffd47354e89f8
Andre Smith <andre.frank.smith@gmail.com>
2022-11-28 20:52:54 +0100

    Modified day 3

--- Day 3 - Array to Number Problem/script.js
+++ Day 3 - Array to Number Problem/script.js
@@ -1,3 +1,15 @@
+/* 
+Challenge #3: Array to Number Problem
+============================
+==> Create a function toArray() that takes any positive number and returns an array of its digits.
+
+Test Data: 
+toArray(235) -> [2, 3, 5]
+toArray(0) -> [0]
+toArray(12) -> [1, 2]
+
+*/
+
 function toArray(number) {
     return number.toString().split('').map(Number);
 }
